3c5a1e7cd5550dce7b39ecac7d35d931cb4a1207
Harshika <harshikasaini1415@gmail.com>
2023-05-25 19:16:25 +0530

    init :initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const birthdate = document.getElementById("birthdayDate");
+const luckynum = document.getElementById("luckyNum");
+const checkbutton= document.getElementById("check");
+const finalResult= document.getElementById("output"); 
+
+function CheckingNumberLuckyOrNot(){
+    const dob = birthdate.value;
+    const sum = calculate(dob); 
+    CompareValues(sum , luckynum.value);
+}
+
+function CompareValues(sum, luckyNum){
+ if(sum%luckyNum===0){
+    finalResult.innerText="Your Birth Date is lucky"
+ }
+ else{
+    finalResult.innerText="Your Birth Date is not lucky";
+ }
+}
+
+function calculate(dob){
+ dob=dob.replaceAll("-","");
+ let sum= 0;
+ for(let i=0; i<dob.length; i++){
+    sum= sum+ Number(dob.charAt(i));
+ }
+ return sum;
+}
+
+
+checkbutton.addEventListener("click",  CheckingNumberLuckyOrNot);
